9204bfdf426622df74b583c28b8fd81287865932
Luis Abreu <luisandre_69@hotmail.com>
2022-08-08 23:16:42 +0100

    first test

---
+++ cypress/e2e/examples/simple.cy.js
@@ -0,0 +1,9 @@
+describe("test 1", () => {
+  it("true should be true", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("true cant be false", () => {
+    expect(true).to.equal(false);
+  });
+});
