350a73dae43ed469c05d4206562452b160b6b552
Luis Abreu <luisandre_69@hotmail.com>
2022-08-09 15:01:47 +0100

    cypress basics

--- cypress/e2e/examples/login.cy.js
+++ cypress/e2e/examples/login.cy.js
@@ -1,18 +1,25 @@
 describe('working with inputs', () => {
+	it('should override the current time', () => {
+		const date = new Date(2022, 8, 8).getTime(); // return a timestamp
+		cy.clock(date);
+		cy.log(date);
+	});
+
 	it('should load login page', () => {
 		cy.visit('http://zero.webappsecurity.com/login.html', { timeout: 10000 });
+		cy.clearCookies({ log: true });
+		cy.clearLocalStorage('your item', { log: true });
+		cy.title().should('include','Zero - Log in')
 	});
 
 	it('should fill username', () => {
 		cy.get('#user_login').as('username');
 		cy.get('@username').clear().type('test', { delay: 50 });
-		// cy.get('@username').type('test', { delay: 50 });
 	});
 
 	it('should fill password', () => {
 		cy.get('#user_password').as('password');
 		cy.get('@password').clear().type('test', { delay: 50 });
-		// cy.get('@password').type('test', { delay: 50 });
 	});
 
 	it('should mark checkbox', () => {
